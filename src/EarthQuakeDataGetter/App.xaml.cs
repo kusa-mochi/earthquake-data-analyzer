40f5e8daa68d4f2e179c50dd4b5aa1ab2d70c484
kusa-mochi <whoatemyapplepie@gmail.com>
2019-10-29 23:18:31 +0900

    国内地震情報収集ツールの新規プロジェクトを追加した。

---
+++ src/EarthQuakeDataGetter/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using EarthQuakeDataGetter.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace EarthQuakeDataGetter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ src/EarthQuakeDataGetter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace EarthQuakeDataGetter.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "国内地震情報収集ツール";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
